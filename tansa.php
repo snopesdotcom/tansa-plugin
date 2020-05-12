cc3232b067f9245717ad4afd794f45d61a7b80ce
Alejandro Giraldo <alejandro@giraldopineda.com>
2020-05-12 18:08:21 -0500

    - Changes suggested by faishal, (several escaping issues)

--- tansa.php
+++ tansa.php
@@ -4,7 +4,7 @@
  * Plugin URI: "https://wordpress.org/plugins/tansa/"
  * Version: 5.0.1.12
  * Author: Tansa Systems AS
- * Author URI: https://www.tansa.com 
+ * Author URI: https://www.tansa.com
  * Description:TANSA IS AN ADVANCED text proofing system that can process thousands of words per second. Not only will it correct nearly all spelling, usage, style, punctuation and hyphenation errors in the blink of an eye, it also ensures that everyone in your organization follows a common set of rules.
  * License: GPLv2 or later */
 
@@ -23,17 +23,17 @@ $userNameOptionValue = 'username'; // user_login
 function register_tansa_extension_info() {
 	global $serverUrlSettingsFieldId, $licenseKeySettingsFieldId, $readUserNameOptionSettingsFieldId, $tansaDevServerURL, $userNameOptionValue ;
 
-	$tansaFolderName = 'Tansa'; 
-	$licenseKey =  get_option($licenseKeySettingsFieldId); 
+	$tansaFolderName = 'Tansa';
+	$licenseKey =  get_option($licenseKeySettingsFieldId);
 	$tansaServerURL = get_option($serverUrlSettingsFieldId);
 	$readUserNameOption = get_option($readUserNameOptionSettingsFieldId);
 
 	if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
-		$tansaIniFilePath = getenv('programdata') . DIRECTORY_SEPARATOR . $tansaFolderName . DIRECTORY_SEPARATOR . 'TS4.ini'; 
+		$tansaIniFilePath = getenv('programdata') . DIRECTORY_SEPARATOR . $tansaFolderName . DIRECTORY_SEPARATOR . 'TS4.ini';
 		if (file_exists ($tansaIniFilePath)) {
 			$tansaIniFileData = parse_ini_file($tansaIniFilePath, true);
-			$licenseKey = $tansaIniFileData['License']['key']; 
-			$tansaServerURL = reset($tansaIniFileData['WebClient']); 
+			$licenseKey = $tansaIniFileData['License']['key'];
+			$tansaServerURL = reset($tansaIniFileData['WebClient']);
 		}
 	}
 
@@ -46,18 +46,18 @@ function register_tansa_extension_info() {
 	if($readUserNameOption == $userNameOptionValue){
 		$wpUserId = $current_user -> data -> user_login;
 	}
-	
-	wp_register_script('register_tansa_extension_info', null); 
+
+	wp_register_script('register_tansa_extension_info', null);
 	$variables = array (
-		'wpVersion' => get_bloginfo('version'), 
-		'version' => '5.0.1.12', 
-		'parentAppLangCode' => get_locale(), 
-		'wpUserId' => $wpUserId, 
-		'licenseKey' => $licenseKey, 
+		'wpVersion' => get_bloginfo('version'),
+		'version' => '5.0.1.12',
+		'parentAppLangCode' => get_locale(),
+		'wpUserId' => $wpUserId,
+		'licenseKey' => $licenseKey,
 		'tansaServerURL' => $tansaServerURL,
 		'pluginPath' => plugins_url('dist', __FILE__));
-	wp_localize_script('register_tansa_extension_info', 'tansaExtensionInfo', $variables); 
-	wp_enqueue_script('register_tansa_extension_info'); 
+	wp_localize_script('register_tansa_extension_info', 'tansaExtensionInfo', $variables);
+	wp_enqueue_script('register_tansa_extension_info');
 }
 
 
@@ -84,22 +84,22 @@ function check_user_access() {
 function load_tansa_gutenberg() {
 
 	if(check_user_access()){
-		$blockPath = '/dist/gutenberg/sidebar.js'; 
-		$stylePath = '/dist/gutenberg/sidebar.css'; 
-	
+		$blockPath = '/dist/gutenberg/sidebar.js';
+		$stylePath = '/dist/gutenberg/sidebar.css';
+
 		register_tansa_extension_info();
 		register_common_js_tansa();
 		// Enqueue the bundled block JS file
-		wp_enqueue_script('tansa-block-js', 
-			plugins_url($blockPath, __FILE__), 
-			[ 'wp-i18n', 'wp-blocks', 'wp-edit-post', 'wp-element', 'wp-editor', 'wp-components', 'wp-data', 'wp-plugins', 'wp-edit-post', 'wp-api'], 
-			filemtime(plugin_dir_path(__FILE__) . $blockPath)); 
-	
+		wp_enqueue_script('tansa-block-js',
+			plugins_url($blockPath, __FILE__),
+			[ 'wp-i18n', 'wp-blocks', 'wp-edit-post', 'wp-element', 'wp-editor', 'wp-components', 'wp-data', 'wp-plugins', 'wp-edit-post', 'wp-api'],
+			filemtime(plugin_dir_path(__FILE__) . $blockPath));
+
 		// Enqueue frontend and editor block styles
-		wp_enqueue_style('tansa-block-css', 
-			plugins_url($stylePath, __FILE__), 
-			'', 
-			filemtime(plugin_dir_path(__FILE__) . $stylePath)); 
+		wp_enqueue_style('tansa-block-css',
+			plugins_url($stylePath, __FILE__),
+			'',
+			filemtime(plugin_dir_path(__FILE__) . $stylePath));
 
 	}
 }
@@ -111,30 +111,30 @@ function load_tansa_gutenberg() {
 function load_tansa_tinymce() {
 
 	if(check_user_access()){
-		register_tansa_extension_info(); 
-		add_filter('mce_external_plugins', 'add_tansa_plugin'); 
+		register_tansa_extension_info();
+		add_filter('mce_external_plugins', 'add_tansa_plugin');
 		add_filter('mce_buttons', 'add_tansa_toolbar_button');
 	}
 }
 
 /**
-* Adds a TinyMCE plugin compatible JS file to the TinyMCE Visual Editor instance *  
-* @param array $plugin_array Array of registered TinyMCE Plugins 
+* Adds a TinyMCE plugin compatible JS file to the TinyMCE Visual Editor instance *
+* @param array $plugin_array Array of registered TinyMCE Plugins
 * @return array Modified array of registered TinyMCE Plugins */
 function add_tansa_plugin($plugin_array) {
-	register_common_js_tansa(); 
-	$plugin_array['tansa'] = plugin_dir_url(__FILE__) . 'dist/tinymce/plugin.min.js'; 
-	return $plugin_array; 
+	register_common_js_tansa();
+	$plugin_array['tansa'] = plugin_dir_url(__FILE__) . 'dist/tinymce/plugin.min.js';
+	return $plugin_array;
 }
 
 /**
-* Adds a button to the TinyMCE Visual Editor which the user can click 
-* to insert a link with a custom CSS class .  *  
-* @param array $buttons Array of registered TinyMCE Buttons 
+* Adds a button to the TinyMCE Visual Editor which the user can click
+* to insert a link with a custom CSS class .  *
+* @param array $buttons Array of registered TinyMCE Buttons
 * @return array Modified array of registered TinyMCE Buttons */
 function add_tansa_toolbar_button($buttons) {
 	array_push($buttons, 'tansaButton');
-	return $buttons; 
+	return $buttons;
 }
 
 function tansa_plugin_create_menu() {
@@ -213,39 +213,39 @@ function field_callback( $arguments ) {
     // Check which type of field we want
     switch( $arguments['type'] ){
 		case 'text': // If it is a text field
-			printf( '<input name="%1$s" id="%1$s" type="%2$s" placeholder="%3$s" title="%3$s" value="%4$s" class="regular-text" />', $arguments['uid'], $arguments['type'], $arguments['placeholder'], $value );
+			printf( '<input name="%1$s" id="%1$s" type="%2$s" placeholder="%3$s" title="%3$s" value="%4$s" class="regular-text" />', esc_attr( $arguments['uid'] ), esc_attr( $arguments['type'] ), esc_attr( $arguments['placeholder'] ), esc_attr( $value ) );
 			break;
 		case 'textarea': // If it is a textarea
-			printf( '<textarea name="%1$s" id="%1$s" placeholder="%2$s" rows="5" cols="50">%3$s</textarea>', $arguments['uid'], $arguments['placeholder'], $value );
+			printf( '<textarea name="%1$s" id="%1$s" placeholder="%2$s" rows="5" cols="50">%3$s</textarea>', esc_attr( $arguments['uid'] ), esc_attr( $arguments['placeholder'] ), esc_textarea( $value ));
 			break;
 		case 'select': // If it is a select dropdown
 			if( ! empty ( $arguments['options'] ) && is_array( $arguments['options'] ) ){
 				$options_markup = ’;
 				foreach( $arguments['options'] as $key => $label ){
-					$options_markup .= sprintf( '<option value="%s" %s>%s</option>', $key, selected( $value, $key, false ), $label );
+					$options_markup .= sprintf( '<option value="%s" %s>%s</option>', esc_attr( $key ), selected( $value, $key, false ), esc_html( $label ));
 				}
-				printf( '<select name="%1$s" id="%1$s">%2$s</select>', $arguments['uid'], $options_markup );
+				printf( '<select name="%1$s" id="%1$s">%2$s</select>', esc_attr( $arguments['uid'] ), esc_html( $options_markup ) );
 			}
 			break;
 		case 'radio': // If it is a radio button
 			if( ! empty ( $arguments['options'] ) && is_array( $arguments['options'] ) ){
 				$options_markup = '';
 				foreach( $arguments['options'] as $key => $optionValue ){
-					$options_markup .= sprintf('<li><input name="%1$s" type="%2$s" title="%3$s" value="%4$s" id="%4$s" %5$s /> <label for="%4$s" style="vertical-align: initial;" >%6$s</label> </li>', $arguments['uid'], $arguments['type'], $arguments['placeholder'], $optionValue, checked($optionValue, $value, false), $key);	
+					$options_markup .= sprintf('<li><input name="%1$s" type="%2$s" title="%3$s" value="%4$s" id="%4$s" %5$s /> <label for="%4$s" style="vertical-align: initial;" >%6$s</label> </li>', esc_attr( $arguments['uid'] ), esc_attr( $arguments['type'] ), esc_attr( $arguments['placeholder'] ), esc_attr( $optionValue ), checked($optionValue, $value, false), esc_html( $key ));
 				}
-				printf('<ul style="margin:0px;">%1$s</ul>', $options_markup);
+				printf('<ul style="margin:0px;">%1$s</ul>', esc_html( $options_markup ) );
 			}
 			break;
 	}
 
     // If there is help text
     if( $helper = $arguments['helper'] ){
-        printf( '<span class="helper"> %s</span>', $helper ); // Show it
+        printf( '<span class="helper"> %s</span>', esc_html( $helper ) ); // Show it
     }
 
     // If there is supplemental text
     if( $supplimental = $arguments['supplemental'] ){
-        printf( '<p class="description">%s</p>', $supplimental ); // Show it
+        printf( '<p class="description">%s</p>', esc_html( $supplimental ) ); // Show it
     }
 }
 
